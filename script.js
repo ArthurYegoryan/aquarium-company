43d8858f00f79ec7c2b5618d4aadafd58de6cbf5
ArthurYegoryan <arthur.yegoryan.97@gmail.com>
2023-12-01 22:02:05 +0400

    fix(img): convert all images to webp, feat(style.css): add styles for header and story part, fix(index.html): fix structure

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 20,
-    stretch: -20,
-    depth: 120,
-    modifier: 3,
-    slideShadows: true,
-  },
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 20,
+        stretch: -20,
+        depth: 120,
+        modifier: 3,
+        slideShadows: true,
+    },
 });
