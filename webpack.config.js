a0245d17d6e34680eedd31dacea32cf7a5ee1708
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-12-02 02:44:54 +0400

    rename(index.html): rename to contactUs.html and change entry point in webpacl.confog.js

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, "index.html"),
+            template: path.resolve(__dirname, "contactUs.html"),
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
